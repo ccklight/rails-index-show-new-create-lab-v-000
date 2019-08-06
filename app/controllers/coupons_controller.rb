f3bd39ac2cd65187a667ceb0943c2b0784c7cadb
github username <github email address>
2019-08-06 09:23:33 -0500

    Done.

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -1,22 +1,29 @@
 class CouponsController < ApplicationController 
 
     def index 
+        @coupons = Coupon.all
         #All of the coupons stored in instance variable
     end  
 
-    def show 
+    def show
         #Isolate/find individual coupon by id
+        @coupon = Coupon.find(params[:id]
     end 
 
     def new 
     end
 
     def create
-        # Instance of coupon = a new coupon 
+        # Instance of coupon = a new coupon
+        @coupon = Coupon.new 
         #Pull params from coupon_code 
+        @coupon[:coupon_code] = params[:coupon][:coupon_code]
         #Pull params from coupon_store
+        @coupon[:store] = params[:coupon][:store] 
         # Save the instance of the coupon
+        @coupon.save
         #Re-direct user to coupon_path
+        redirect_to coupon_path(@coupon) 
     end
 
 
